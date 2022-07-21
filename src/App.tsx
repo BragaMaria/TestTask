15bca20c7dd3d7d84ac1ff65a4203687e1c44ecf
BragaMaria <brag4.m@yandex.com>
2022-07-21 10:56:24 +0500

    Интерфес с параметрами

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+interface IData {
+  item: {
+    id: string;
+    title: string;
+    content: string;
+  };
+}
 
 function App() {
+  const post = [
+    {
+      id: "1",
+      title: "Отдых в июле на Черном море",
+      content: " ",
+    },
+  ];
   return (
     <div className="App">
       <h3>Проект </h3>
-      <div className='post'>Блок 1</div>
+      <div className="post">Блок 1</div>
     </div>
   );
 }
+const elems = post.map((elem) => {
+  return <Elem item={elem} />;
+});
+<div>{elems}</div>;
 
 export default App;
