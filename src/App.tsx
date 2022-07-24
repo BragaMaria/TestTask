2f1dcb1b84ce5b5acea224c9ca4969382c9adabd
BragaMaria <brag4.m@yandex.com>
2022-07-24 20:35:15 +0500

    Вывод  всех данных с помощью функции map()

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+interface IData {
+  id: number;
+  title: string;
+  description: string;
+}
+
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState<IData[]>();
 
   useEffect(() => {
     fetch("http://localhost:3005/posts")
@@ -15,33 +21,25 @@ function App() {
       });
   }, []);
 
-  console.log(data)
-
   return (
     <div className="App">
-      <div className="post">
-        <div className="text">
-          <h3 className="title">1. Заголовок поста</h3>
-          <p className="description">Описание поста</p>
-        </div>
-        <div className="buttons">
-          <input type="button" value="Удалить" />
-          <input type="button" value="Редактировать" />
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="text">
-          <h3 className="title">2. Заголовок поста</h3>
-          <p className="description">Описание поста</p>
-        </div>
-        <div className="buttons">
-          <input type="button" value="Удалить" />
-          <input type="button" value="Редактировать" />
-        </div>
-      </div>
+      {data?.map((post) => {
+        return (
+          <div className="post" key={post.id}>
+            <div className="text">
+              <h3 className="title">
+                {post.id}. {post.title}
+              </h3>
+              <p className="description">{post.description}</p>
+            </div>
+            <div className="buttons">
+              <input type="button" value="Удалить" />
+              <input type="button" value="Редактировать" />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
-
 export default App;
