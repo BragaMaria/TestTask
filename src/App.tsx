d59136025f9e81c348cf8d71c3b1c3149c16b4df
BragaMaria <brag4.m@yandex.com>
2022-07-27 10:59:57 +0500

    Кнопка редактировать функциональна

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,48 @@ interface IData {
 function App() {
   const [data, setData] = useState<IData[]>();
 
+  const [bodyEdit, setBodyEdit] = useState({
+    title: "",
+    description: "",
+  });
+
+  const [openForm, setOpenForm] = useState(false);
+
+  const [idForm, setIdForm] = useState(0);
+
   const getData = () => {
     return fetch("http://localhost:3005/posts").then((result) => {
       return result.json();
     });
   };
 
-  
   useEffect(() => {
     getData().then((result) => {
       setData(result);
     });
   }, []);
 
-  
   const remove = (id: number) => {
     fetch(`http://localhost:3005/posts/${id}`, {
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
+    })
+      .then((result) => {
+        return result.json();
+      })
+      .then(() => {
+        getData().then((result) => {
+          setData(result);
+        });
+      });
+  };
+
+  const edit = (id: number, e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch(`http://localhost:3005/posts/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(bodyEdit),
     })
       .then((result) => {
         return result.json();
@@ -52,16 +76,64 @@ function App() {
               <p className="description">{post.description}</p>
             </div>
             <div className="buttons">
-              <input type="button" value="Удалить" onClick={()=>{remove(post.id)}}/>
-              <input type="button" value="Редактировать" />
+              <input
+                type="button"
+                value="Удалить"
+                onClick={() => {
+                  remove(post.id);
+                }}
+              />
+              <input
+                type="button"
+                value="Редактировать"
+                onClick={() => {
+                  setOpenForm(true);
+                  setIdForm(post.id);
+                }}
+              />
             </div>
           </div>
         );
       })}
-      <input className="edit" type="text" />
-      <input className="edit" type="text" />
-      <input className="edit_button" type="button" value="Редактировать пост"/>
 
+      
+      {openForm && (
+        <div className="form-edit">
+          <form
+            onSubmit={(e) => {
+              edit(idForm, e);
+            }}
+          >
+            <input
+              className="edit"
+              type="text"
+              onChange={(e) => {
+                setBodyEdit({
+                  title: e.target.value,
+                  description: bodyEdit.description,
+                });
+              }}
+            />
+
+            <input
+              className="edit"
+              type="text"
+              onChange={(e) => {
+                setBodyEdit({
+                  title: bodyEdit.title,
+                  description: e.target.value,
+                });
+              }}
+            />
+
+            <input
+              className="edit-button"
+              type="submit"
+              value="Редактировать пост"
+            />
+          </form>
+        </div>
+      )}
     </div>
   );
 }
